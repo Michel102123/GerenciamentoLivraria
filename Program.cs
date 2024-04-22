f26d4268e71ee2b6181604dae9da01816334c099
Michel102123 <michel.jean.santos@escola.pr.gov.br>
2024-04-22 11:07:11 -0300

    exercicio livro

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace livraria;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Livros livroBirdBox= new Livros();
+		
+		
+		livroBirdBox.Settitulo("Titulo: Bird Box");
+		
+		
+		
+		
+		
+		Console.WriteLine(livroBirdBox.GetNome());
+		Console.WriteLine(livroBirdBox.GetAutor());
+		Console.WriteLine(livroBirdBox.GetEstoque());
+		Console.WriteLine(livroBirdBox.GetIsbn());
+	}
+}
+
